835b7246498df51d8641a76912f57da408799b99
shahinBS023 <shahin.brainstation23@gmail.com>
2022-08-16 13:21:24 +0600

    Add TestNG parallel runner

--- src/test/java/TestRunner/MyTestRunner.java
+++ src/test/java/TestRunner/MyTestRunner.java
@@ -1,13 +1,14 @@
 package TestRunner;
 
 
+import Steps.MyAppHook;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/Steps"},
+        features = {"src/test/resources/Steps/"},
         glue = {"Steps"},
         plugin = {"pretty",
                 "html:test-output/cucumber-report/nopCommerce-pretty.html",
--- src/test/resources/Steps/SearchProduct.feature
+++ src/test/resources/Steps/SearchProduct.feature
@@ -4,4 +4,4 @@ Feature: Search a product
     Given Goto home page-for search
     Then Give product name for search "Apple MacBook Pro 13-inch"
     And click the search button
-    Then Check the search product "Apple MacBook Pro hi hello 13-inch"
+    Then Check the search product "Apple MacBook Pro hi 13-inch"
