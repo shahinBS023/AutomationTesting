da4ca94f8bf8756f1776358ac90a8c128e00fcb3
shahinBS023 <shahin.brainstation23@gmail.com>
2022-08-16 14:52:24 +0600

    Add failes scenario rerun option

---
+++ src/test/java/Steps/ParallelRunner.java
@@ -0,0 +1,28 @@
+package Steps;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+
+@CucumberOptions(
+        plugin = {"pretty",
+                "html:test-output/cucumber-report/nopCommerce-pretty.html",
+                "json:test-output/cucumber-report/nopCommerce.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "rerun:target/failedScenario.txt"
+        },
+        monochrome = true,
+        glue = { "Steps" },
+        features = { "src/test/resources/Steps" }
+
+)
+
+
+public class ParallelRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
--- src/test/java/Steps/ReRunner.java
+++ src/test/java/Steps/ReRunner.java
@@ -4,21 +4,17 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-
 @CucumberOptions(
         plugin = {"pretty",
-                "html:test-output/cucumber-report/nopCommerce-pretty.html",
-                "json:test-output/cucumber-report/nopCommerce.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+                "rerun:target/failedScenario.txt"
         },
         monochrome = true,
         glue = { "Steps" },
-        features = { "src/test/resources/Steps" }
+        features = { "@target/failedScenario.txt" }
 
 )
 
-
-public class parallelRunner extends AbstractTestNGCucumberTests {
+public class ReRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
